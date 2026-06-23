909f7242ac3c1e92b346831eb42f8ee03bf920fa
Rgem <UgwuRhema@proton.me>
2026-06-23 05:14:51 +0100

    Removed something

--- TempleOS/build.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-x86_64 -cdrom TOS_Distro.ISO -hda temple.qcow2 -m 1024 -smp 4 -boot order=d -usb -device usb-tablet -display gtk
--- TempleOS/build2.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-x86_64 -hda temple.qcow2 -m 2046 -smp 4 -boot order=d -usb -device usb-tablet -display gtk
